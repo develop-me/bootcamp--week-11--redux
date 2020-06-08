938e72b5dd2ea254d1a7a80568b615e3f9212786
Mark Wales <mark@smallhadroncollider.com>
2020-06-08 15:15:32 +0100

    update: updated notes/scripts to use new Restful.training auth

--- quiz/answers/src/axios.js
+++ quiz/answers/src/axios.js
@@ -3,11 +3,9 @@ import axios from "axios";
 
 // return a new version of axios with useful settings applied
 export default axios.create({
-    baseURL: "http://test.restful.training/api/tasks",
-    params: {
-        key: "88cd89cd70dfeffd0ad109b912f7eb98b814a140",
-    },
+    baseURL: "https://restful.training/api/tasks",
     headers: {
         Accept: "application/json",
+        Authorization: "Bearer 88cd89cd70dfeffd0ad109b912f7eb98b814a140",
     },
 });
